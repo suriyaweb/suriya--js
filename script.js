00eb67b5be604767bc7f4bb49f1bcd0c97ef378f
suriyaweb <suris3631@gmail.com>
2024-10-18 00:40:05 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,354 @@
+// console.log("ghgg");
+//  var
+
+// variable declaration
+// var string = "suriya"; 
+// /variable redeclaration
+// var strin1 ="kumar";
+//   strin1 ="fdhffgfgfgffgg";
+
+//   reassignment
+// console.log(string,strin1)
+
+
+// let
+// let name ="ghgg";
+//  name = 123454666;
+//  console.log(name);
+
+//  const str = "hufhg"
+//  str ="hjhhj";
+//  console.log(str)
+//  var name="suriya";
+//  var name="suriya";
+//  let age="23";
+//   age="24";
+//  console.log(age);
+// let n = 'hghguhgufgyufyuy';
+// console.log(typeof n);
+
+
+// let data = suriya,madurai,tn;
+// console.log(data);
+// let data  =["name","age","city","state"]
+// console.log(data)
+// let person= {
+//     name :"suriya",
+//     age :"age",
+//     mark :"80",
+//     city :"madurai",
+// }
+// console.log(person)
+// function addNumbers(a, b) {
+//     return a + b;
+// }
+
+// let result = addNumbers(5, 10);
+// console.log(result);  // Output: 15
+
+// function addNumbers(a,b) {
+//     return a + b;
+// }
+// var submit = addNumbers(20,30);
+// console.log(submit);
+// function 
+// let x=5;
+// let y=5;
+// // addition
+//  console.log(x+y);
+//  console.log(x-y);
+//  console.log(x*y);
+//  console.log(x%y);
+//  console.log(x/y);
+//  console.log(x++);
+//   console.log(x++);
+//   console.log(y--);
+// console.log(y--);
+// assigment operter
+// let x='10';
+// console.log(typeof x)
+// console.log(x=10)
+// console.log(x+=5)
+// console.log(x-=3)
+// console.log(x*=2)
+// let number = 20;
+// let number =0;
+//     if (number < 0){
+//          console.log ( "The number is positive.");
+//     }
+//     else if (number < 0){
+//         console.log( "The number is negative.");
+//     }
+//    else(number < 0);{
+//        console.log( "The number is zero.");
+//     }
+// let day =1
+// switch(day =3){
+//     case 1:
+//     console.log("monday");
+//     break;
+//     case 2:
+//     console.log("tuseday");
+//     break;
+//     case 3:
+//     console.log("wednesday");
+//     break;
+// case4 :
+// console.log("thuseday");
+// break;
+// case 5:
+// console.log("friday");
+// break;
+// case 6:
+// console.log("saturday");
+// break;
+// case 7:
+// console.log("sunday");
+// break;
+// }
+// for(let i=1; i <=20; i++){
+//      console.log(i);
+// }
+// let i = 10;
+
+// while (i >= 2) {
+//      console.log(i);
+// i--;
+//  }
+// for(let i=10; i >=0; i--){
+//     console.log(i);
+// }
+// for(let i =0; i <=10; i++){
+//     console.log(i)
+// }
+// for(let i = 1; i <= 5; i++){
+//     console.log(i*2)
+
+
+//the number is :1
+//the number is :2
+//the number is :3
+//the number is :4
+//the number is :5
+//  for(let i = 1; i <= 5; i++){
+//     console.log(  "the number is" +i)
+//   }
+// let i =1;
+//    while (i < 10) {
+//       console.log(i);
+//  i++;
+//   }
+
+// for (let i = 1; i <= 15; i++) {
+//     if (i % 2 == 0)
+//         console.log(i);
+// }
+// let i = 1;
+// while (i < 15){
+//     if ( i % 3 == 0){
+//         console.log(i)
+//     }
+//     i++;
+    
+// }
+
+// let i =1;
+// do{
+//     if ( i % 2 == 0)
+//     console.log(i)
+// i++;
+// }
+// while(i > 15)
+
+//named 
+// function sayhello(){
+//      console.log("hello")
+//  }
+//  sayhello()
+//  //anony
+//  let hisuriya = function(){
+//      console.log("hisuriya");
+//  }
+//  console.log(hisuriya);
+ 
+// //arrow
+// const add = (a , b) => 44+33;
+// console.log(add(3,4));
+
+// //iife
+//  (function(a, b) {
+//     console.log(a * b); 
+//  })(10 , 20);
+
+//stringmethods
+var str ="hello world"
+ var res = str . charAt(7);
+console.log(res)
+// //charcodeat
+var str = "hi suriya"
+var res = str . charCodeAt(0);
+console.log(res)
+//concat
+var str1 = "hello";
+var str2 = "world";
+var res = str1.concat(str2);
+console.log(res)
+//endswith
+var str="hi suriya how are you";
+var res=str.endsWith("you");
+console.log(res)
+//includes
+var str="mr suriya crpf in deffence";
+var res=str.includes("crpf")
+console.log(res)
+//indexof
+var str="hello suriya ";
+var res= str.indexOf("suriya");
+console.log(res)
+//lastindex
+var str="india england america poland nethaland";
+var res= str.lastIndexOf("america");
+console.log(res);
+//repeat
+var str="hello! suriya";
+console.log(str.repeat(2))
+//replace
+var str="visit microsofe";
+var res = str.replace("microsofe","suirya");
+console.log(res)
+//search
+var str="hi suriya good";
+var res=str.search("suriya");
+console.log(res)
+//slice
+var str="hi suriya good";
+var res=str.slice(0,7);
+console.log(res)
+//split
+var str="how are you suriya";
+var res= str.split(" ");
+console.log(res)
+//to lowercase
+var str="Hi i am suriya ";
+var res=str.toLowerCase();
+console.log(res)
+//touppercase
+var str="hello world";
+var res=str.toUpperCase();
+console.log(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
